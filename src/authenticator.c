d8bae326bb900fbbc9c2235a8b63a2e4c0c5932a
Matthew Connelly <github@maff.im>
2016-04-19 20:04:53 +0100

    more debugging

--- src/authenticator.c
+++ src/authenticator.c
@@ -67,7 +67,11 @@ static uint32_t get_token(time_t time_utc) {
 	uint8_t offset = s.state.b[HASH_LENGTH-1]; offset &= 0x0F;
 	uint32_t otp = 0;
 	APP_LOG(APP_LOG_LEVEL_DEBUG,"offset %X",(unsigned int)offset);
-	for(int i = 0;i<4;i++) APP_LOG(APP_LOG_LEVEL_DEBUG,"byte %i %X",offset+i,(unsigned int)s.state.b[offset+i]);
+	int i = 0;
+	APP_LOG(APP_LOG_LEVEL_DEBUG,"byte %i %X",offset+i,(unsigned int)(s.state.b[offset+i] & 0x7F) << 24);
+	i = 1; APP_LOG(APP_LOG_LEVEL_DEBUG,"byte %i %X",offset+i,(unsigned int)s.state.b[offset+i] << 16);
+	i = 2; APP_LOG(APP_LOG_LEVEL_DEBUG,"byte %i %X",offset+i,(unsigned int)s.state.b[offset+i] << 8);
+	i = 3; APP_LOG(APP_LOG_LEVEL_DEBUG,"byte %i %X",offset+i,(unsigned int)s.state.b[offset+i]);
 	//We then truncate
 	//our OTP is (the byte at [offset] left-shift 24 AND 0x7F) OR ([offset+1] left-shift 16) OR ([offset+2] left-shift 8) OR [offset+3] 
 	otp = ((s.state.b[offset] & 0x7f) << 24) | (s.state.b[offset + 1] << 16) | (s.state.b[offset + 2] << 8) | s.state.b[offset + 3];
